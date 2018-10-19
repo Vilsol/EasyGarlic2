7bdef72754e7fdc1ddc00c10736b81f7a53bcdff
PandawanFr <pandawanfr@gmail.com>
2018-10-18 18:07:15 -0700

    Electron + React + Typescript

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import App from 'App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from 'registerServiceWorker';
+
+import 'index.css';
+
+ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+registerServiceWorker();
