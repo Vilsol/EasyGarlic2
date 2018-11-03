41e0ab50c14961aeb8512cd41cfd512224b65709
Vilsol <me@vil.so>
2018-11-03 21:51:46 +0200

    Build Windows inside docker

---
+++ scripts/build-linux.sh
@@ -0,0 +1,5 @@
+docker run --rm \
+    --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
+    -v ${PWD}:/project \
+    electronuserland/builder:wine \
+    /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn run electron-pack-docker"
