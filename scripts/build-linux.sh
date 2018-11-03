ecf2c5b227598609877912f36236b2bb0de2b7c8
Vilsol <me@vil.so>
2018-11-03 21:55:23 +0200

    Pack electron inside docker

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -2,4 +2,4 @@ docker run --rm \
     --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
     -v ${PWD}:/project \
     electronuserland/builder:wine \
-    /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn run electron-pack-docker"
+    /bin/bash -c "yarn --link-duplicates --pure-lockfile && yarn run preelectron-pack && yarn run electron-pack-docker"
