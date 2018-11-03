465df8dd20b4b07fa1b1d4b68297f29b8334311a
Vilsol <me@vil.so>
2018-11-03 22:04:05 +0200

    Only pre-pack in osx. Fix categories

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker run --rm \
     --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS|APPVEYOR_|CSC_|_TOKEN|_KEY|AWS_|STRIP|BUILD_') \
     -v ${PWD}:/project \
